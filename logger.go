3cfbe02049b148689e200b2e62ad05ddad1c89cb
pscompsci <pstacey@deakin.edu.au>
2021-07-25 10:53:38 +1000

    Updated logger file and tests

--- logger.go
+++ logger.go
@@ -2,128 +2,146 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"sync"
 	"time"
 )
 
+type Level int
+
 const (
-	debug = iota
-	info
-	warn
-	err
-	fatal
+	Debug Level = iota
+	Info
+	Warn
+	Err
+	Fatal
 )
 
-type Logger struct {
+func (l Level) String() string {
+	return [...]string{"DEBUG", "INFO ", "WARN ", "ERROR", "FATAL"}[l]
+}
+
+type Logger interface {
+	SetLevel(level string)
+	SetTimeFormat(timeformat string)
+	LogDebug(message string)
+	LogInformation(message string)
+	LogWarning(message string)
+	LogError(mesage string)
+	LogFatal(message string)
+}
+
+type FileLogger struct {
 	filename   string
 	mutex      *sync.Mutex
-	level      int
+	level      Level
 	timeformat string
 }
 
-// NewLogger returns a new Logger instance
-func NewLogger(filename string, timeformat string) *Logger {
-	l := &Logger{
+// NewFileLogger returns a Logger that logs to file
+func NewFileLogger(filename string, timeformat string) *FileLogger {
+	fl := &FileLogger{
 		filename:   filename,
 		mutex:      &sync.Mutex{},
-		level:      info,
+		level:      Info,
 		timeformat: timeformat,
 	}
-	return l
-}
-
-// SetTimeFormat sets the timeformat for the log messages
-func (l *Logger) SetTimeFormat(timeformat string) {
-	l.timeformat = timeformat
+	return fl
 }
 
 // SetLevel sets the minimum logging level (defaul = INFORMATION)
-func (l *Logger) SetLevel(level string) {
+func (fl *FileLogger) SetLevel(level string) {
 	switch level {
 	case "DEBUG":
-		l.level = debug
+		fl.level = Debug
 	case "INFORMATION":
-		l.level = info
+		fl.level = Info
 	case "WARNING":
-		l.level = warn
+		fl.level = Warn
 	case "ERROR":
-		l.level = err
+		fl.level = Err
 	case "FATAL":
-		l.level = fatal
+		fl.level = Fatal
 	default:
-		l.level = info
+		fl.level = Info
 	}
+}
+
+// SetTimeFormat sets the timeformat for the log messages
+func (fl *FileLogger) SetTimeFormat(timeformat string) {
+	fl.timeformat = timeformat
 }
 
 // LogDebug writes a debug message to the log file
-func (l *Logger) LogDebug(msg string) {
-	l.log(debug, msg)
+func (fl *FileLogger) LogDebug(msg string) {
+	fl.logToFile(Debug, msg)
 }
 
 // LogInformation writes an information message to the log file
-func (l *Logger) LogInformation(msg string) {
-	l.log(info, msg)
+func (fl *FileLogger) LogInformation(msg string) {
+	if fl.checkLevel(Info) {
+		fl.logToFile(Info, msg)
+	}
 }
 
 // LogWarning logs a warning message to the log file
-func (l *Logger) LogWarning(msg string) {
-	l.log(warn, msg)
+func (fl *FileLogger) LogWarning(msg string) {
+	if fl.checkLevel(Warn) {
+		fl.logToFile(Warn, msg)
+	}
 }
 
 // LogError logs an error message to the log file
-func (l *Logger) LogError(msg string) {
-	l.log(err, msg)
+func (fl *FileLogger) LogError(msg string) {
+	if fl.checkLevel(Err) {
+		fl.logToFile(Err, msg)
+	}
 }
 
 // LogFatal logs a fatal message to the log file
-func (l *Logger) LogFatal(msg string) {
-	l.log(fatal, msg)
+func (fl *FileLogger) LogFatal(msg string) {
+	if fl.checkLevel(Fatal) {
+		fl.logToFile(Fatal, msg)
+	}
 }
 
-func (l *Logger) log(level int, message string) {
-	if level < l.level {
-		return
+func (fl *FileLogger) checkLevel(level Level) bool {
+	if level < Debug || level > Fatal {
+		return false
 	}
+	return fl.level <= level
+}
 
-	l.mutex.Lock()
-	defer l.mutex.Unlock()
+func (fl *FileLogger) logToFile(level Level, message string) {
+	fl.mutex.Lock()
+	defer fl.mutex.Unlock()
 
-	fmtMsg := fmt.Sprintf("%s %s: %s\n", time.Now().Format(l.timeformat), levelToString(level), message)
+	msg := formatMessage(time.Now(), fl.timeformat, level.String(), message)
 
-	logFile, err := os.OpenFile(l.filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+	f, err := os.OpenFile(fl.filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
 	if err != nil {
-		os.Stderr.WriteString(fmt.Sprintf("logger: %s\n", fmtMsg))
+		os.Stderr.WriteString(fmt.Sprintf("logger: Could not open or create log file\n%s\n", msg))
 	}
 
 	defer func() {
-		err := logFile.Close()
+		err := f.Close()
 		if err != nil {
-			os.Stderr.WriteString(fmt.Sprintf("logger: Closing failed on log file: %s\n", l.filename))
+			os.Stderr.WriteString(fmt.Sprintf("logger: Closing failed on log file: %s\n", fl.filename))
 			return
 		}
 	}()
 
-	if _, err := logFile.WriteString(fmtMsg); err != nil {
-		os.Stderr.WriteString(fmt.Sprintf("logger: Writing failed on log file: %s\n", l.filename))
-		return
-	}
-
+	log(f, msg)
 }
 
-func levelToString(level int) string {
-	switch level {
-	case debug:
-		return "DEBUG"
-	case info:
-		return "INFO "
-	case warn:
-		return "WARN "
-	case err:
-		return "ERROR"
-	case fatal:
-		return "FATAL"
-	default:
-		return "INFO "
+func formatMessage(time time.Time, timeformat string, level string, message string) string {
+	return fmt.Sprintf("%s %s: %s", time.Format(timeformat), level, message)
+}
+
+func log(logFile io.Writer, fmtMsg string) error {
+	if _, err := logFile.Write([]byte(fmtMsg)); err != nil {
+		return err
 	}
+	return nil
 }
--- logger_test.go
+++ logger_test.go
@@ -1,32 +1,116 @@
 package main
 
 import (
+	"bytes"
+	"strings"
 	"testing"
 	"time"
 )
 
-func TestNewLoggerCreatesOK(t *testing.T) {
-	l := NewLogger("log_test.log", time.RFC3339)
-	level := l.level
-	if level != info {
-		t.Fatalf("Logger level incorrect. Want: 1, Got: %d", level)
+func TestNewFileLogger(t *testing.T) {
+	fl := NewFileLogger("log_test.log", time.RFC3339)
+	level := fl.level
+	if level != Info {
+		t.Errorf("Logger level incorrect. Want: 1, Got: %d", level)
 	}
 }
 
-func TestSetLogLevelOK(t *testing.T) {
-	l := NewLogger("log_test.log", time.RFC3339)
-	l.SetLevel("FATAL")
-	level := l.level
-	if level != fatal {
-		t.Fatalf("Logger level incorrect. Want: 4, Got: %d", level)
+func TestSetLogLevel(t *testing.T) {
+	type test struct {
+		level string
+		want  Level
+	}
+
+	tests := []test{
+		{want: Debug, level: "DEBUG"},
+		{want: Info, level: "INFORMATION"},
+		{want: Warn, level: "WARNING"},
+		{want: Err, level: "ERROR"},
+		{want: Fatal, level: "FATAL"},
+		{want: Info, level: "INVALID"},
+	}
+
+	fl := NewFileLogger("test_log.log", time.RFC3339)
+
+	for _, tc := range tests {
+		fl.SetLevel(tc.level)
+		if fl.level != tc.want {
+			t.Fatalf("expected: %v, got: %v", tc.want, fl.level)
+		}
 	}
 }
 
-func TestSetLogLevelDefaultsToInfoOnInvalidLevel(t *testing.T) {
-	l := NewLogger("log_test.log", time.RFC3339)
-	l.SetLevel("NOT_A_REAL_LEVEL")
-	level := l.level
-	if level != info {
-		t.Fatalf("Logger level incorrect. Want: 1, Got: %d", level)
+func TestCheckLevel(t *testing.T) {
+	type test struct {
+		level Level
+		want  bool
+	}
+
+	tests := []test{
+		{level: Debug, want: true},
+		{level: Info, want: true},
+		{level: Warn, want: true},
+		{level: Err, want: true},
+		{level: Fatal, want: true},
+		{level: 10, want: false},
+	}
+
+	fl := NewFileLogger("test_log.log", time.RFC3339)
+	fl.SetLevel("DEBUG")
+
+	for _, tc := range tests {
+		got := fl.checkLevel(tc.level)
+		if got != tc.want {
+			t.Errorf("CheckLevel: Want: '%v', Got: '%v'", tc.want, got)
+		}
+	}
+}
+
+func TestLevelString(t *testing.T) {
+	type test struct {
+		level Level
+		want  string
+	}
+
+	tests := []test{
+		{level: Debug, want: "DEBUG"},
+		{level: Info, want: "INFO "},
+		{level: Warn, want: "WARN "},
+		{level: Err, want: "ERROR"},
+		{level: Fatal, want: "FATAL"},
+	}
+
+	for _, tc := range tests {
+		got := tc.level.String()
+		if got != tc.want {
+			t.Fatalf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
+
+func TestFormatMessage(t *testing.T) {
+	str := "2021-07-25T00:00:00.000Z"
+	testTime, err := time.Parse(time.RFC3339, str)
+	if err != nil {
+		t.Errorf("Could not parse string to time")
+	}
+	levelString := "DEBUG"
+	testMsg := "Test message"
+	msg := formatMessage(testTime, time.RFC3339, levelString, testMsg)
+	want := "2021-07-25T00:00:00Z DEBUG: Test message"
+	if strings.Compare(msg, want) != 0 {
+		t.Errorf("FormatTime: Want: '%s', Got: '%s'", want, msg)
+	}
+}
+
+func TestLog(t *testing.T) {
+	var b bytes.Buffer
+	want := "Test log message"
+	if err := log(&b, want); err != nil {
+		t.Fatalf("Could not log message")
+	}
+	got := b.String()
+	if got != want {
+		t.Errorf("log: Want: %s, Got: %s", want, got)
 	}
 }
